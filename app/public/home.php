ca6d30f2110950d1fc181504998de1d556a539e5
vuntsova <vuntsova@aol.com>
2017-06-07 21:15:44 -0400

    index

---
+++ app/public/home.php
@@ -0,0 +1,17 @@
+<!-- PHP	 -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<!-- <p>Hello World!</p> -->
+<!-- to use PHP change it prom index.html to index.php -->
+<?php
+echo 'Hello World';
+$name = 'Hoba';
+// TIP: USE '' insted of ""
+echo $name;
+?>
+</body>
+</html>
